be7d0a8937ec6cd0be347fa2394fcc948fba6203
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 22:25:04 +0500

    three number

---
+++ Task013/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Определение третьей цифры числа - Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = (number / 100) % 10;
+
+if (result == 0)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine($"{number} -> {result}");
+}
