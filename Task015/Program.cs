44d9c0c514e2f156ec4d425ce75ac81f3ae18bcd
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 22:51:38 +0500

    day of the week

---
+++ Task015/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Определение формата дня недели - Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = (number / 100) % 10;
+
+if (number <= 7)
+{
+    if(number >= 6)
+    {
+        Console.WriteLine($"{number} день недели -> выходной");
+    }
+
+    else
+    {
+        Console.WriteLine($"{number} день недели -> рабочий");
+    }
+}    
+else
+{
+    Console.WriteLine($"Не верно введённые данные, в неделе 7 дней");
+}
